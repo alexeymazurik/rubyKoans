57f5bdf1d169a42ae74407fb6ba5a992ad6f5a97
Alexey Mazurik <alexeymazurik@gmail.com>
2014-10-08 21:59:01 +0300

    Done

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,8 +30,36 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+
+  points = 0;
+  counter = 0;
+
+  if dice.length >= 3
+    dice.sort!
+    if (dice[0] ==  1 && dice [2] == 1)
+      points += 1000;
+      counter += 3
+    elsif (dice[0] == dice[2])
+      points +=dice[0]*100
+      counter += 3
+    end
+  end
+
+  while counter <= dice.length do 
+    if dice[counter] == 5
+      points += 50
+    elsif dice[counter] == 1
+      points += 100
+    end
+
+    counter += 1
+
+  end
+
+  points
+        
 end
+
 
 class AboutScoringProject < Neo::Koan
   def test_score_of_an_empty_list_is_zero
--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,43 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	
+
+	# fail TriangleError if (a+b) <=c
+	
+	# case [a, b, c].uniq.size
+	# when 1 then :equilateral
+	# when 2 then :isosceles
+	# else		:scalene
+
+	# 	nil
+
+sizes = [a, b, c].sort
+
+raise TriangleError if sizes[0] + sizes[1] <= sizes[2] 
+raise TriangleError if sizes[0] <= 0
+
+case sizes.uniq.size
+when 1 then :equilateral
+when 2 then :isosceles
+else		:scalene
+end
+
+ #  abc = [a, b, c].sort!
+
+ #  raise TriangleError if abc[0] <= 0 
+ #  raise TriangleError if abc[0] + abc[1] <= abc[2] 
+
+	# if abc.uniq.length == 1
+	# 	:equilateral
+	# 	elsif abc.uniq.length == 2
+	# 		:isosceles
+	# 	else
+	# 		:scalene
+	# end	
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
 end
